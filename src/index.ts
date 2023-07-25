4969c1b704208018e2b4254cc68c289017f0d86e
DasminX <wojciech.szpila.dev@gmail.com>
2023-07-25 17:48:02 +0200

    css adjust, subpage, dynamic aos dataset etc.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 function initApp() {
+  const presentationals = Array.from(
+    document.querySelectorAll("p[class*='__presentation__']")
+  ) as HTMLParagraphElement[];
+  if (Array.isArray(presentationals) && presentationals.length > 0) {
+    for (let i = 0; i < presentationals.length; i++) {
+      presentationals[i].dataset.aos = "fade-right";
+      presentationals[i].dataset.aosDuration = "550";
+      presentationals[i].classList.add("aos-init");
+    }
+  }
   if (typeof AOS === "object" && typeof AOS.init === "function") {
-    AOS.init()
+    AOS.init();
   }
 }
 
-window.addEventListener("DOMContentLoaded", initApp)
+window.addEventListener("DOMContentLoaded", initApp);
