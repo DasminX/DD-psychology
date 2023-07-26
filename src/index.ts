6127fef20f3871f9a759c104817ec6147c10af1a
DasminX <wojciech.szpila.dev@gmail.com>
2023-07-26 21:35:30 +0200

    motto/opis

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 function initApp() {
-  const presentationals = Array.from(
+  /*   const presentationals = Array.from(
     document.querySelectorAll("p[class*='__presentation__']")
   ) as HTMLParagraphElement[];
   if (Array.isArray(presentationals) && presentationals.length > 0) {
@@ -8,7 +8,7 @@ function initApp() {
       presentationals[i].dataset.aosDuration = "550";
       presentationals[i].classList.add("aos-init");
     }
-  }
+  } */
   if (typeof AOS === "object" && typeof AOS.init === "function") {
     AOS.init();
   }
