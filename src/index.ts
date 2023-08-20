67e27e20e077719d8b78c98da5ee318a113d007a
DasminX <wojciech.szpila.dev@gmail.com>
2023-08-20 11:50:38 +0200

    desktop view initial - to be fixed, adjustments, nav highlighting

--- src/index.ts
+++ src/index.ts
@@ -20,28 +20,32 @@ function initApp() {
   initNavigation();
 
   function initAos() {
-    if (typeof AOS === "object" && typeof AOS.init === "function") {
-      AOS.init();
-    }
+    try {
+      if (typeof AOS === "object" && typeof AOS.init === "function") {
+        AOS.init();
+      }
+    } catch (e) {}
   }
 
   function initSwiper() {
-    if (typeof Swiper === "function") {
-      const swiperCont = document.querySelector(".swiper") as unknown as HTMLDivElement;
-      if (!swiperCont) return;
+    try {
+      if (typeof Swiper === "function") {
+        const swiperCont = document.querySelector(".swiper") as unknown as HTMLDivElement;
+        if (!swiperCont) return;
 
-      const swiper = new Swiper(".swiper", {
-        speed: 600,
-        spaceBetween: 48,
-        loop: true,
-        centeredSlides: true,
-        touchStartForcePreventDefault: true,
-        setWrapperSize: true,
-        autoplay: {
-          delay: 3000,
-        },
-      });
-    }
+        const swiper = new Swiper(".swiper", {
+          speed: 600,
+          spaceBetween: 48,
+          loop: true,
+          centeredSlides: true,
+          touchStartForcePreventDefault: true,
+          setWrapperSize: true,
+          autoplay: {
+            delay: 3000,
+          },
+        });
+      }
+    } catch (e) {}
   }
 
   function initNavigation() {
@@ -49,6 +53,33 @@ function initApp() {
     const navToggle = document.querySelector(".nav__toggle") as unknown as HTMLDivElement;
     const navList = document.querySelector(".nav__list") as unknown as HTMLUListElement;
     if (!nav || !navToggle || !navList) return; // set desktop view
+    highlightNavLink();
+
+    function highlightNavLink() {
+      try {
+        const { pathname } = location;
+        const idSelector = pathname.slice(1).trim() || "home";
+        const cont = navList.querySelector(`#navitem-${idSelector}`);
+        if (!cont) return;
+        cont.classList.add("current-tab");
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+
+    function changeNavColor() {
+      if (nav.classList.contains("nav__active")) {
+        nav.classList.add("scrolled");
+        return;
+      }
+      if (nav.classList.contains("scrolled")) {
+        if (window.scrollY > window.outerHeight / 10) return;
+        nav.classList.remove("scrolled");
+      } else {
+        if (window.scrollY <= window.outerHeight / 10) return;
+        nav.classList.add("scrolled");
+      }
+    }
 
     navToggle.addEventListener("click", (e: Event) => {
       if (nav.classList.contains("nav__active")) {
@@ -64,24 +95,12 @@ function initApp() {
       if (!(e.target instanceof Element)) return;
       if (!e.target.closest("li")) return;
 
+      highlightNavLink();
+
       nav.classList.remove("nav__active");
     });
 
     window.addEventListener("scroll", changeNavColor);
-
-    function changeNavColor() {
-      if (nav.classList.contains("nav__active")) {
-        nav.classList.add("scrolled");
-        return;
-      }
-      if (nav.classList.contains("scrolled")) {
-        if (window.scrollY > window.outerHeight / 10) return;
-        nav.classList.remove("scrolled");
-      } else {
-        if (window.scrollY <= window.outerHeight / 10) return;
-        nav.classList.add("scrolled");
-      }
-    }
   }
 }
 
